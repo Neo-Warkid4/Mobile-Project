5ac34e9340321304d5e9ff6c9a4e2501d66bb1a7
Neo-Warkid4 <33049886+Neo-Warkid4@users.noreply.github.com>
2017-10-27 12:16:42 +1000

    Added Basic Attractor Script

--- Assets/Scripts/Attractor.cs
+++ Assets/Scripts/Attractor.cs
@@ -6,9 +6,27 @@ public class Attractor : MonoBehaviour {
 
 	public Rigidbody rb;
 
-	void Attract (Attractor objToAttract)
+	void FixedUpdate ()
 	{
-		
+		Attractor[] attractors = FindObjectsOfType<Attractor>();
+		foreach (Attractor attractor in attractors)
+		{
+			Attract(attractor);
+
+		}
 	}
 
+	void Attract (Attractor objToAttract)
+	{
+		Rigidbody rbToAttract = objToAttract.rb;
+
+		Vector3 direction = rb.position - rbToAttract.position;
+		float distance = direction.magnitude;
+
+		float forceMagnitude = (rb.mass * rbToAttract.mass) / Mathf.Pow(distance, 2);
+		Vector3 force = direction.normalized * forceMagnitude;
+
+		rbToAttract.AddForce(force);
+	}
+		
 }
