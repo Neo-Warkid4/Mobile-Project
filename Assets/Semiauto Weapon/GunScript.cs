7314a05917749144f08d347e59fdb11a9a296fef
Jackson Edwards <33049886+Neo-Warkid4@users.noreply.github.com>
2017-11-21 15:00:19 +1000

    fps gun creation

---
+++ Assets/Semiauto Weapon/GunScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GunScript : MonoBehaviour
+{
+    public Transform barrel;
+    public float range = 0f;
+
+    void Update ()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            StartCoroutine("Fire");
+        }
+    }
+
+    IEnumerator Fire ()
+    {
+        RaycastHit hit;
+        Ray ray = new Ray(barrel.position, transform.forward);
+
+        if (Physics.Raycast(ray, out hit, range))
+        {
+            if (hit.collider.tag == "Enemy")
+            {
+                Enemy enemy = hit.collider.GetComponent<enemy>();
+                enemy.health -= 1;
+            }
+        }
+
+        Debug.DrawRay(barrel.position, transform.forward * range, Color.green);)
+        yield return null;
+    }
+}
